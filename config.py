0ff81684e119f8b014bb20f01cd5e4b9a6c0e7ba
Mohammad Munim <122454355+munim-de@users.noreply.github.com>
2025-07-28 15:12:06 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    EMAIL_USER = os.getenv('EMAIL_USER')
+    EMAIL_PASS = os.getenv('EMAIL_PASS')
+    ALERT_RECEIVER = os.getenv('ALERT_RECEIVER')
