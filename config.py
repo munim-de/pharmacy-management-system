7b1e63586878bad8f95760e0988690f41d854975
Mohammad Munim <122454355+munim-de@users.noreply.github.com>
2025-07-28 16:41:19 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     EMAIL_USER = os.getenv('EMAIL_USER')
     EMAIL_PASS = os.getenv('EMAIL_PASS')
     ALERT_RECEIVER = os.getenv('ALERT_RECEIVER')
